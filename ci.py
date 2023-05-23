0ae3f0f780836fbaf9fcbc8d51270f6a932ac730
Han <50775491+seunghwan1228@users.noreply.github.com>
2023-05-23 16:09:58 +0900

    Update ci.py

--- ci.py
+++ ci.py
@@ -3,4 +3,4 @@ import wandb
 print(f'Wandb Version {wandb.__version__}')
 
 
-assert wandb.__version == '2.2.2', f'The version of wandb should be 2.2.2 not {wandb.__version__}'
+assert wandb.__version__ == '2.2.2', f'The version of wandb should be 2.2.2 not {wandb.__version__}'
